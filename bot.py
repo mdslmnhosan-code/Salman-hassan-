944452caa7903c4f776d5b79f922aa2e3404f0c7
mdslmnhosan-code <mdslmnhosan@gmail.com>
2026-07-12 17:47:53 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -3044,7 +3044,14 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
 
     print("🚀 BOT RUNNING...")
-    app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
+    PORT = int(os.environ.get("PORT", "8443"))
+app.run_webhook(
+    listen="0.0.0.0",
+    port=PORT,
+    webhook_url="https://salman-hassan.onrender.com",
+    allowed_updates=Update.ALL_TYPES
+)
+
 
 if __name__ == "__main__":
     main()()
